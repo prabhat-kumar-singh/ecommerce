ac42a71cc5cd660d1ec98a5e59448918924dcdb0
Prabhat <prabhatkumarsingh462@gmail.com>
2023-06-17 14:58:55 +0530

    frontend folder structure

---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css"
+
+
+const Home = () => {
+    return(
+        <div>
+            <p>Welcome Home</p>
+        </div>
+    )
+}
+
+export default Home;
